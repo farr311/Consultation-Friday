3b1ecab1e126a605e26da1aaab6f0e8114f711c6
farr311 <40473601+farr311@users.noreply.github.com>
2023-01-13 13:12:13 +0200

    Fat cats

--- src/consult13_01/Main.java
+++ src/consult13_01/Main.java
@@ -1,13 +1,31 @@
 package consult13_01;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        Cat cat1 = new Cat("Scottish fold", 5, 4.4, "Light gray");
-        Cat cat2 = new Cat("Scottish straight", 7, 6.4, "Black");
+        double weight = scanner.nextDouble();
+        String breed = scanner.next();
+        String color = scanner.next();
+        int age = scanner.nextInt();
+
+        if (weight > 6) {
+            System.out.println("Too fat!");
+        }
+
+        Cat cat1 = new Cat(breed, age, weight, color);
 
         System.out.println(cat1);
-        System.out.println(cat2);
+
+        /*Cat cat1 = new Cat("Scottish fold", 5, 4.4, "Light gray");
+        Cat cat2 = new Cat("Scottish straight", 7, 6.4, "Black");
+
+        Cat cat3 = new Cat("Scottish straight", 3, 6.4, "Black");
+
+        System.out.println(cat1);
+        System.out.println(cat2);*/
     }
 }
